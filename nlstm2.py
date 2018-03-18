116592abff83c3e5a057bac4704261e3278f83a4
Abduallah Mohamed <abduallah.adel.omar@gmail.com>
2018-03-18 17:40:05 -0500

    Update nlstm2.py

--- nlstm2.py
+++ nlstm2.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-
+from torch.autograd import Variable
 class LSTMCell(nn.Module):
 
     def __init__(self, input_size, hidden_dim, bias=True):
